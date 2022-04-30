c57c0806ce20b712774497595a7428ffea71add2
David Roddy <davidroddy321@gmail.com>
2022-04-29 17:53:45 -0700

    makes a circle of pins, still offset though

--- cones/runner.py
+++ cones/runner.py
@@ -1,9 +1,9 @@
 from hedgehog import hedgehog
 
 
-h = hedgehog(0.7, 4, 8, "test", d_across_pinbase=1)
+h = hedgehog(0.7, 4, 8, "test")
 
 # h.viewDetails()
-#h.generateGDML(4)
+h.generateGDML(4, 2)
 #h.gdml2f()
 h.gdml2stl()
