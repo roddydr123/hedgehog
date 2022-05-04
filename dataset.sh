45f063a117f0c25ef6628de59b5c74cdcf7b4caa
David Roddy <davidroddy321@gmail.com>
2022-05-03 17:36:34 -0700

    remove spare scripts

--- dataset.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-MATRIX="/mnt/c/users/david/documents/triumf/projects/3drm-design/hedgehog/cones/matrix"
-
-for i in {1..16..1}
-do
- printf -v j "%02d" $i
- scp -i ~/.ssh/id_rsa droddy@fluka02.triumf.ca:/group/pifnif/david/dataset3/pm$j/plot05.dat $MATRIX/pm$j.txt
-done
