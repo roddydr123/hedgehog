7e7335eefae1df02a7121ff8c02ee4a7f49b0cfb
David Roddy <davidroddy321@gmail.com>
2022-05-03 17:25:59 -0700

    adding bash scripts

---
+++ dataset.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/bash
+
+MATRIX="/mnt/c/users/david/documents/triumf/projects/3drm-design/hedgehog/cones/matrix"
+
+for i in {1..16..1}
+do
+ printf -v j "%02d" $i
+ scp -i ~/.ssh/id_rsa droddy@fluka02.triumf.ca:/group/pifnif/david/dataset3/pm$j/plot05.dat $MATRIX/pm$j.txt
+done
