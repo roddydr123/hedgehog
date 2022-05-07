28e2f872230808719d23928cbcecba13197b8485
David Roddy <davidroddy321@gmail.com>
2022-05-06 17:49:34 -0700

    adding intensity.py, semi-works

---
+++ cones/intensity.py
@@ -0,0 +1,26 @@
+import numpy as np
+import sys
+from weightsCone import path
+
+
+def main():
+
+    no_bins = 200
+    header = 2
+
+    print("reading in...")
+
+    data = np.genfromtxt(f'{path}data/{sys.argv[1]}', skip_header=header)
+
+    fludet1slice = data[:no_bins]
+    fludet2slice = data[602:802]
+    fludet1 = sum(fludet1slice[:, 2])
+    fludet2 = sum(fludet2slice[:, 2])
+    print(fludet1 * 100 / fludet2)
+
+    arr = fludet1slice[fludet1slice[:, 2] > 0]
+    print(np.average(arr[:, 3] / arr[:, 2]))
+    print(fludet1slice[:, 2].max())
+
+
+main()
