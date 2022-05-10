f92a4fdc98ccef7b95e4d8573bb613a1f695e92f
David Roddy <davidroddy321@gmail.com>
2022-05-10 15:15:52 -0700

    add beamline input template

--- cones/runner.py
+++ cones/runner.py
@@ -1,9 +1,9 @@
 from hedgehog import hedgehog
 
 
-h = hedgehog(0.7, 4, 8, "test", d_across_pinbase=1)
+h = hedgehog(1, 4, 9, "reduced-5mm", d_across_pinbase=0.5)
 
 # h.viewDetails()
-#h.generateGDML(4)
-#h.gdml2f()
-h.gdml2stl()
+h.generateGDML(3)
+h.gdml2f()
+#h.gdml2stl()
