12118e28b21f6747dc3cc0d4e0fd761511544b40
David Roddy <davidroddy321@gmail.com>
2022-06-08 14:18:35 -0400

    updated plots

--- cones/runner.py
+++ cones/runner.py
@@ -1,9 +1,9 @@
 from hedgehog import hedgehog
 
 
-h = hedgehog(1.5, 4.0, 15, "hedgehogJ3", tolerance=1E-6)
+h = hedgehog(1, 4.0, 50, "hedgehogJ2a", usrWeights=[10, 1, 1], tolerance=1E-3)
 
-#h.viewDetails()
-h.generateGDML(5, 2.3)
-h.gdml2f()
+h.viewDetails()
+#h.generateGDML(5, 2.3)
+#h.gdml2f()
 #h.gdml2stl()
