2b30dcb70fe230e140bab99928ed13f1d30c5d8b
David Roddy <davidroddy321@gmail.com>
2022-06-21 12:06:14 -0700

    orientation graph made now

--- cones/plotboth.py
+++ cones/plotboth.py
@@ -4,6 +4,7 @@ import numpy as np
 from private.private import path
 import pandas as pd
 from scipy.integrate import simpson
+from SOBPwidth import getwidth
 
 
 plt.style.use('/mnt/c/users/david/documents/triumf/poster/poster.mplstyle')
@@ -13,30 +14,38 @@ def sobp():
     dataArr = []
     filenames = []
 
+    lines = ["k-", "r-", "k--", "r--"]
+
     # load in all the files and store their names
     for file in sys.argv[1:]:
         dataArr.append(np.array(loader(file)))
         filenames.append(file)
 
-    #filenames = ["Simulation result", "Resin small printer", "Resin industrial printer", "Nylon filament"]
-    #filenames = ["Simulation result", "Wide pins (7mm)", "Medium pins (5mm)", "Thin pins (3mm)"]
-    #filenames = ["Predicted by optimizer", "Simulation result"]
-
     fig, ax = plt.subplots()
+    filenamess = ["Normal measured", "Reversed measured", "Normal simulated", "Reversed simulated"]
 
-    #ax1 = ax.twinx()
+    measured_avgs = []
+
+    low = 2.7
+    high = 3.5
     #low, high = input("please input the range for normalisation: ").split(' ')
 
     for i, sobp in enumerate(dataArr):
+
+        # change units if needed
+        #while sobp[0][-1] > 11:
+        #    sobp[0] /= 10
 
         # scale in x due to slabs angle
         diff = 0.5
         # angle = np.degrees(np.arctan(diff / 20))
         hyp = np.sqrt(diff**2 + 20**2)
         scaler = hyp / 20
-        if "film" in filenames[i]:
+
+        if ".csv" in filenames[i]:
             thresh = 0.4
             sobp[0] *= scaler
+            sobp[0] /= 10
         else:
             thresh = 0.1
 
@@ -44,36 +53,42 @@ def sobp():
         a = np.argmax(sobp[1] > thresh * sobp[1].max())
         sobp[0] -= sobp[0][a]
 
-        # change units if needed
-        if sobp[0][-1] > 10:
-            sobp[0] /= 10
+        # while sobp[1].max() > 20:
+        #    sobp[1] /= 10
 
-        # proper proper normalisation
-        area = simpson(sobp[1], sobp[0])
-        sobp[1] /= area
+        slice = (sobp[0] >= float(low)) & (sobp[0] <= float(high))
 
-        if i == 0:
+        # normalisation not great for films
+        # area = simpson(sobp[1], sobp[0])
+        # sobp[1] /= area
+
+        measured_avgs.append(np.average(sobp[1][slice]))
+
+        if i == 2:
+            sobp[1] /= sobp[1].max() / measured_avgs[0]
+
+        if i == 3:
+            sobp[1] /= sobp[1].max() / measured_avgs[1]
+
+        getwidth(sobp[0], sobp[1])
+
+        if i == 50:
             ax.plot(sobp[0], sobp[1], label=filenames[i], color="k")
         #    #ax1.plot(sobp[0], sobp[1], label=filenames[i], color="k")
         else:
             #ax.plot(sobp[0], sobp[1], label=filenames[i], linestyle="dashed", color="k")
-            ax.plot(sobp[0], sobp[1], label=filenames[i])
-
+            ax.plot(sobp[0], sobp[1], lines[i], label=filenamess[i])
         #ax.plot(sobp[0], sobp[1], label=filenames[i])
 
     ax.legend()
-    #ax1.legend()
     ax.set_xlabel("Depth in water (cm)")
-    ax.set_ylabel("Normalised dose")
-    #ax.set_ylabel("Normalised pixel value (films)")
+    ax.set_ylabel("Dose (Gy)")
     ax.set_xlim(0, 5)
 
     #ax.set_ylim(-0.1, 1.1)
-    #ax1.set_ylim(-0.1, 1.1)
 
     ax.xaxis.set_tick_params(length=6, width=2)
     ax.yaxis.set_tick_params(length=6, width=2)
-    #ax1.yaxis.set_tick_params(length=6, width=2)
 
     plt.tight_layout()
 
@@ -89,7 +104,7 @@ def profile():
         dataArr.append(np.array(loader(file)))
         filenames.append(file)
 
-    filenames = ["Reverse J2", "J2", "J5", "No HEDGEHOG"]
+    #filenames = ["Reverse J2", "J2", "J5", "No HEDGEHOG"]
     lines = ["solid", "dotted", "dashed", "dashdot"]
 
     fig, ax = plt.subplots()
@@ -106,10 +121,10 @@ def profile():
         profile[1] /= area
 
         # find >99%
-        slice = profile[1] >= 0.95 * profile[1].max()
+        slice = profile[1] >= 0.90 * profile[1].max()
         print(profile[0][slice][-1] - profile[0][slice][0])
 
-        ax.plot(profile[0], profile[1], label=str(filenames[i]), color="k", linestyle=lines[i])
+        ax.plot(profile[0], profile[1], label=str(filenames[i]), color="k")
 
     ax.legend()
     ax.set_xlabel("Distance from maximum (cm)")
