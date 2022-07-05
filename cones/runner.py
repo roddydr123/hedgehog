3837008908bd47f1dd3132345f1ad67a2d3d1f30
David Roddy <davidroddy321@gmail.com>
2022-07-04 22:15:34 -0700

    analyse minimum data

--- cones/runner.py
+++ cones/runner.py
@@ -1,9 +1,25 @@
 from hedgehog import hedgehog
+from private.private import path
+import numpy as np
 
 
-h = hedgehog(1.0, 4.0, 8, "hedgehogJ5", tolerance=1E-3)
+h = hedgehog(1.5, 4.0, 12, "test", tolerance=1E-4)
 
-#h.viewDetails()
+h.viewDetails()
 #h.generateGDML(5, 2.3)
 #h.gdml2f()
-h.gdml2stl()
+#h.gdml2stl()
+
+
+def analyse():
+    with open(path+"minsum - Copy.txt", "r") as file:
+        lines = file.readlines()
+    lis = []
+    for line in lines:
+        lis.append(float(line))
+
+    lis = np.array(lis)
+
+    print(np.std(lis))
+    print(np.average(lis))
+    print(len(lis))
