7b0c2785a5190755287cfcb6750c15dd5bfbb7df
roddydr123 <davidroddy321@gmail.com>
2025-12-07 14:46:28 +0000

    add optimizer test

--- tests/test_initial.py
+++ tests/test_initial.py
@@ -1,7 +1,10 @@
 import hedgehog.classes as hog
+from hedgehog.cubic import optimizer
 import pathlib
 import shutil
+import pickle
 from tests.test_utils import util_test_files_identical_line_by_line, stl_same_geometry
+import numpy as np
 
 
 RUN_PATH = pathlib.Path(__file__)
@@ -94,3 +97,29 @@ def test_gdml2stl(tmp_path):
     assert tmp_path.joinpath("test_output.stl").exists()
 
     stl_same_geometry(tmp_path.joinpath("test_output.stl"), RUN_PATH.parent / "test_data" / "test_output.stl")
+
+
+def test_optimizer(tmp_path):
+
+    thicklist = [0.1, 0.19, 0.29, 0.38, 0.47, 0.57, 0.66, 0.75, 0.85, 0.94, 1.03,
+                 1.13, 1.22, 1.31, 1.41, 1.5, 1.6, 1.7, 1.8, 1.9, 2.0, 2.1, 2.2,
+                 2.3, 2.4, 2.5, 2.6, 2.7, 2.8]
+
+    # provide path to underlying sims.
+    sim_path = RUN_PATH.parents[1] / "hedgehog" / "usims"
+    us = hog.undersim(thicklist, sim_path)
+
+    # provide desired SOBP details.
+    sobp = hog.SOBPeak(2.5, 4.0, 10)
+
+    # create hedgehog instance with location to store produced files and convergence point of optimization.
+    h = hog.hedgehog(sobp, us, tmp_path.joinpath("test_output"), tolerance=1E-3)
+
+    pinData = optimizer(h.SOBPeak, h.undersim, h.d_across_pinbase, h.tolerance,
+                        h.usrWeights, h.radius_cutoff, filename=h.filename, show=0)
+
+    with open(RUN_PATH.parent / "test_data" / "pinData.pkl", "rb") as f:
+        exp_pinData = pickle.load(f)
+
+    assert np.all(np.isclose(pinData["radii"], exp_pinData["radii"]))
+    assert np.all(np.isclose(pinData["thicknesses"], exp_pinData["thicknesses"]))
